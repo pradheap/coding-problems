fc0b7a41e3954c02db18f3416a199c8a67cee431
Pradheap <pradheap@gmail.com>
2019-03-13 22:58:20 -0700

    Update Array reverse with recursive solution.

--- array_reverse/source.py
+++ array_reverse/source.py
@@ -3,6 +3,7 @@
 def reverse_array(arr):
     return arr.reverse()
 
+# In place reversing
 def reverse_array_v1(arr):
     left = 0
     right = len(arr) - 1
@@ -10,3 +11,11 @@ def reverse_array_v1(arr):
         arr[left], arr[right] = arr[right], arr[left]
         left += 1
         right -= 1
+
+
+# Returns a reversed copy of arr
+def reverse_array_v2(arr):
+    if len(arr) < 2:
+        return arr
+
+    return [arr[len(arr) - 1]] + reverse_array_v2(arr[1:(len(arr) - 1)]) + [arr[0]]
--- array_reverse/test.py
+++ array_reverse/test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from array_reverse.source import reverse_array, reverse_array_v1
+from array_reverse.source import reverse_array, reverse_array_v1, reverse_array_v2
 
 class TestArrayReverse(unittest.TestCase):
     def test_empty_list(self):
@@ -59,3 +59,26 @@ class TestArrayReverseV1(unittest.TestCase):
         reverse_array_v1(arr)
 
         self.assertListEqual(arr, [5, 3, 2, 1])
+
+class TestArrayReverseV2(unittest.TestCase):
+    def test_empty_list(self):
+        self.assertListEqual([], reverse_array_v2([]))
+
+    def test_unit_list(self):
+        arr = [2]
+
+        self.assertListEqual(reverse_array_v2(arr), [2])
+
+    def test_two_elements_list(self):
+        arr = [1, 2]
+
+        result = reverse_array_v2(arr)
+
+        self.assertListEqual(result, [2, 1])
+
+    def test_multiple_elements_list(self):
+        arr = [1, 2, 3, 5]
+
+        result = reverse_array_v2(arr)
+
+        self.assertListEqual(result, [5, 3, 2, 1])
